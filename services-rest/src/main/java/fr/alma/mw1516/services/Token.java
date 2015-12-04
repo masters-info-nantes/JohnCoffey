41df034eb7988fdd529a9f695d23e5192e8e2539
JulienOuvrard <julien.ouvrard311@gmail.com>
2015-12-04 10:38:13 +0100

    first commit

---
+++ services-rest/src/main/java/fr/alma/mw1516/services/Token.java
@@ -0,0 +1,16 @@
+package fr.alma.mw1516.services;
+
+/**
+ * Created by julien on 01/12/15.
+ */
+public class Token {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
